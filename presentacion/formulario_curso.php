ac8cac1d9862e9b0b0fe700418fca5605cff2a51
Joel Frank <cancerfrlj@gmail.com>
2015-06-13 15:01:13 -0500

    Merge origin/master
    Conflicts:
    	data/usuariodata.php
    	entidad/personaentity.php
    	negocio/login.php
    	negocio/regusuario.php
    	presentacion/listargrupo.php
    	presentacion/listarpersona.php
    	presentacion/listarusuario.php
    	presentacion/regpersona.php

--- presentacion/formulario_curso.php
+++ presentacion/formulario_curso.php
@@ -5,7 +5,7 @@
                   <div class="box-body">
                     <div class="form-group">
                       <label>Nombre Curso</label>
-                      <input type="text" name="id_curso" value="<?php echo $_POST['id_curso']?>"></input>
+                      <input type="hidden" name="id_curso" value="<?php echo $_POST['id_curso']?>"></input>
                       <input type="text" class="form-control" name="descripcion" value="<?php echo $curso->descripcion; ?>">
                     </div>
                     <div class="form-group">
